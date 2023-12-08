c13b3d15e252b241887dc17790499c8828aff730
Mothkuri Prince <130035014+princemothkuri@users.noreply.github.com>
2023-12-08 20:30:41 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "e553538f16msh7d49e81dba9f772p1d9dbbjsnc68b2f88741e", // enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "4a9856691c0700c83f1c3af223ab6d14"; // enter your key from openweather API
